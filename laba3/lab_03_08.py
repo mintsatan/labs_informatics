2db91e8913cc27af4ef04b1008d5e663804b7602
Yana <kittijane@mail.ru>
2019-12-12 16:20:02 +0300

    we are doing something strange

---
+++ laba3/lab_03_08.py
@@ -0,0 +1,10 @@
+"""
+ Анонимные функции, lambda-выражения
+"""
+lfunc = lambda x, y, z = 1: x + y + z
+print("lfunc(1,2,3): ", lfunc(1, 2, 3))
+print("lfunc(1,2): ", lfunc(1, 2))
+print("lfunc(x=1,y=3): ", lfunc(x=1, y=3))
+print("lambda result: ", (lambda a, b, sep=", ": sep.join((a, b)))("Hello", "World!"))
+lam = (lambda x: print(0) if x % 3 == 0 else x)
+lam(int(input()))
